841792f2e3aace8062b15306db31e8480b689bbb
Suraj Kaushik <surajkaushik@ibm.com>
2024-06-03 19:43:00 +0530

    initial commit

---
+++ student/src/test/java/com/suraj/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
